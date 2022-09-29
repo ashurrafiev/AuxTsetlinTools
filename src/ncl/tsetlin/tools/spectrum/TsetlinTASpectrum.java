c731b585855abeaaf3ecc211db5c046456b91dd4
Ashur Rafiev <ashur.rafiev@ncl.ac.uk>
2022-09-29 15:06:03 +0000

    Removed dependency on JDiagram

--- src/ncl/tsetlin/tools/spectrum/TsetlinTASpectrum.java
+++ src/ncl/tsetlin/tools/spectrum/TsetlinTASpectrum.java
@@ -9,15 +9,14 @@ import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
-import com.xrbpowered.jdiagram.data.Data;
-import com.xrbpowered.jdiagram.data.Data.Row;
-
 public class TsetlinTASpectrum {
 
-	public static final String basePath = "../../ClassParallelTM/bak/res0803/3/";
+	public static final String basePath = "../../ClassParallelTM/bak/res0902/1jm/";
 	
 	public static final String dataPathFmt = basePath+"c%d-spectrum.csv";
 	public static final int classes = 10;
@@ -28,7 +27,7 @@ public class TsetlinTASpectrum {
 	public static final String imgPathFmt = basePath+"c%d-spectrum.png";
 	public static final int pointw = 4;
 	public static final int pointh = 2;
-	public static final double normalise = 1500.0; // 1000.0;
+	public static final double normalise = 5000.0; // 1000.0;
 	
 	public static final int marginLeft = 50;
 	public static final int marginRight = 20;
@@ -53,8 +52,25 @@ public class TsetlinTASpectrum {
 	public static void main(String[] args) {
 		for(int cls=0; cls<classes; cls++) {
 			String dataPath = String.format(dataPathFmt, cls);
-			Data data = Data.read(new File(dataPath));
-			int xcount = data.count();
+			
+			ArrayList<String> lines = new ArrayList<>();
+			try {
+				Scanner in = new Scanner(new File(dataPath));
+				in.nextLine();
+				while(in.hasNextLine()) {
+					String line = in.nextLine();
+					if(!line.isEmpty())
+						lines.add(line);
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			int xcount = lines.size();
+			if(xcount==0) {
+				System.err.printf("No data for class %d\n", cls);
+				return;
+			}
 			
 			BufferedImage img = new BufferedImage(pointw*xcount+marginLeft+marginRight, pointh*ycount+marginTop+marginBottom, BufferedImage.TYPE_INT_RGB);
 			Graphics2D g2 = img.createGraphics();
@@ -63,15 +79,11 @@ public class TsetlinTASpectrum {
 			g2.translate(marginLeft, marginTop);
 			
 			int x = 0;
-			for(Row row : data.rows()) {
+			for(String line : lines) {
 				if(x>0) {
+					String[] data = line.split("\\s+");
 					for(int y=0; y<ycount; y++) {
-						String hdr = String.format("s%+d", y+minState);
-						Double v = row.getNum(hdr);
-						if(v==null) {
-							System.out.printf("[%s][%d] is null\n", hdr, x);
-							v = 0.0;
-						}
+						int v = Integer.parseInt(data[y+1]);
 						float c = 1.0f - (float)Math.min(v/normalise, 1.0);
 						g2.setColor(new Color(c, c, c));
 						g2.fillRect(x*pointw, (ycount-y-1)*pointh, pointw, pointh);
@@ -83,7 +95,7 @@ public class TsetlinTASpectrum {
 			int y;
 			g2.setColor(new Color(0x22000000, true));
 			g2.setStroke(new BasicStroke(1));
-			for(x=1; x<=data.count(); x+=xgrid)
+			for(x=1; x<=xcount; x+=xgrid)
 				g2.drawLine(x*pointw, 0, x*pointw, pointh*ycount);
 			for(y=0; y<=ygrid; y++)
 				g2.drawLine(pointw, y*pointh*ycount/ygrid, pointw*xcount, y*pointh*ycount/ygrid);
@@ -98,7 +110,7 @@ public class TsetlinTASpectrum {
 			Font font = new Font("Tahoma", Font.PLAIN, 13);
 			g2.setFont(font);
 			g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
-			for(x=1; x<=data.count(); x+=xgrid)
+			for(x=1; x<=xcount; x+=xgrid)
 				drawString(g2, String.format("%d:%dk", (x-1)/60, (x-1)%60), x*pointw, pointh*ycount+5, 1, 0);
 			drawString(g2, "time (epochs:inputs)", xcount*pointw/2, pointh*ycount+20, 1, 0);
 			for(y=0; y<=ygrid; y++) {
